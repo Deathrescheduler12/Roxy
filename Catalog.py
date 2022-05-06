763fc78e154e33b62058694be1eb988f8c0e322b
Deathrescheduler12 <104992858+Deathrescheduler12@users.noreply.github.com>
2022-05-06 13:17:03 +0300

    Update Catalog.py

--- Catalog.py
+++ Catalog.py
@@ -2,56 +2,89 @@ import requests
 class Catalog:
     def __init__(self, limit: int=60):
         self.limit = str(limit)
-        self.hat = requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=HeadAccessories").json()
-        print(self.hat)
-        self.collectibles = requests.get(f"https://catalog.roblox.com/v1/search/items?category=Collectibles&limit={self.limit}&subcategory=Collectibles").json()
-        self.premium = requests.get(f"https://catalog.roblox.com/v1/search/items?category=Premium&limit={self.limit}&subcategory=Premium").json()
-        self.featured = requests.get(f"https://catalog.roblox.com/v1/search/items?category=Featured&limit={self.limit}&subcategory=Featured").json()
-        self.clothing = requests.get(f"https://catalog.roblox.com/v1/search/items?category=Clothing&limit={self.limit}&subcategory=Clothing").json()
-        self.hair = requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=HairAccessories").json()
-        self.face = requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=FaceAccessories").json()
-        self.neck = requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=NeckAccessories").json()
-        self.shoulder = requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=ShoulderAccessories").json()
-        self.front = requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=FrontAccessories").json()
-        self.back = requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=BackAccessories").json()
-        self.waist = requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=WaistAccessories").json()
     @property
     async def Hats(self):
-        return self.hat
+        try:
+            return self.hats 
+        except:
+            self.hats = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=HeadAccessories").json()["data"]]
+            return self.hats
     @property
     async def Collectibles(self):
-        return self.collectibles
+        try:
+            return self.collectibles 
+        except:
+            self.collectibles = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=Collectibles&limit={self.limit}&subcategory=Collectibles").json()["data"]]
+            return self.collectibles
     @property
     async def Premium(self):
-        return self.premium
+        try:
+            return self.premium 
+        except:
+            self.premium = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=Premium&limit={self.limit}&subcategory=Premium").json()["data"]]
+            return self.premium
     @property
     async def Featured(self):
-        return self.Featured
+        try:
+            self.featured 
+        except:
+            self.featured = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=Featured&limit={self.limit}&subcategory=Featured").json()["data"]]
+            return self.featured
     @property
     async def Clothing(self):
-        return self.clothing
+        try:
+            return self.clothing
+        except:
+            self.clothing = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=Clothing&limit={self.limit}&subcategory=Clothing").json()["data"]]
+            return self.clothing
     @property
     async def Hair(self):
-        return self.hair
+        try:
+            return self.hair
+        except:
+            self.hair = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=HairAccessories").json()["data"]]
     @property
     async def Face(self):
-        return self.face
+        try:
+            return self.face
+        except:
+            self.face = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=FaceAccessories").json()["data"]]
+            return self.face
     @property
     async def Neck(self):
-        return self.neck
+        try:
+            return self.neck
+        except:
+            self.neck = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=NeckAccessories").json()["data"]]
+            return self.neck
     @property
     async def Shoulder(self):
-         return self.shoulder
+        try:
+            return self.shoulder
+        except:
+            self.shoulder = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=ShoulderAccessories").json()["data"]]
+            return self.shoulder
     @property
     async def Front(self):
-         return self.front
+        try:
+            return self.front
+        except:
+            self.front = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=FrontAccessories").json()["data"]]
+            return self.front
     @property
     async def Back(self):
-         return self.back
+        try:
+            return self.back
+        except:
+            self.back = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=BackAccessories").json()["data"]]
+            return self.back
     @property
     async def Waist(self):
-         return self.waist
-
+        try:
+            return self.waist
+        except:
+            self.waist = [x["id"] for x in requests.get(f"https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit={self.limit}&subcategory=WaistAccessories").json()["data"]]
+            return self.waist
 
 
 
